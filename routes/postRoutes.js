b43e231d6ac120494a5570303eae941530f5da30
JuniorCesarMarques <juniorcms1995@gmail.com>
2025-01-20 17:27:06 -0300

    correção camel case

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const authController = require("../controllers/postsController");
 
-router.get("/allPosts", authController.showAllPosts);
+router.get("/all-posts", authController.showAllPosts);
 
 module.exports = router;
