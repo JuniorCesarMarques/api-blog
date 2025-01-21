7eeaabb1231646335d4704e7577fee69d2435553
JuniorCesarMarques <juniorcms1995@gmail.com>
2025-01-21 17:42:37 -0300

    correção dos nomes dos aquivos controllers

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controllers/jsadnkas");
+const authController = require("../controllers/PostController");
 
 router.get("/all-posts", authController.showAllPosts);
 
